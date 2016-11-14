0f2529c418daeb5c4ccb68735cb5a9ef36a11745
Suleiman <suleiman@56060.in>
2016-11-14 10:46:23 +0530

    auto logging network calls

--- app/src/main/java/com/suleiman/pagination/api/MovieApi.java
+++ app/src/main/java/com/suleiman/pagination/api/MovieApi.java
@@ -1,5 +1,7 @@
 package com.suleiman.pagination.api;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -13,9 +15,17 @@ public class MovieApi {
 
     private static Retrofit retrofit = null;
 
+    private static OkHttpClient buildClient() {
+        return new OkHttpClient
+                .Builder()
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .build();
+    }
+
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
+                    .client(buildClient())
                     .addConverterFactory(GsonConverterFactory.create())
                     .baseUrl("https://api.themoviedb.org/3/")
                     .build();
